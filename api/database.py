1ac4ff42dacf1a76454926dfc769b8496960c097
Kirill Kazakov <kazakovkirilliy@gmail.com>
2022-08-22 21:14:29 +0200

    fix: if not exists

--- api/database.py
+++ api/database.py
@@ -6,7 +6,9 @@ from . import settings
 DATABASE_URL = f'postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 
 engine = create_engine(DATABASE_URL)
-engine.execute('CREATE EXTENSION postgis;')
+engine.execute('CREATE EXTENSION IF NOT EXISTS postgis;')
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
