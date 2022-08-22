30d91d4f8defe26e8e53c0b479e2d89dbfaef981
Kirill Kazakov <kazakovkirilliy@gmail.com>
2022-08-22 20:50:15 +0200

    fix: create extension postgis

--- api/database.py
+++ api/database.py
@@ -6,6 +6,7 @@ from . import settings
 DATABASE_URL = f'postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 
 engine = create_engine(DATABASE_URL)
+engine.execute('CREATE EXTENSION postgis;')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
